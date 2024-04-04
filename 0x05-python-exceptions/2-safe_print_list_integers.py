30d9cb441fea324fc0ac0b0321a1cce28153f698
Babaoni147 <obahedwin@ymail.com>
2024-04-04 05:13:34 +0100

    correction

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
-def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
+def safe_print_list_integers(my_list=[], x=0):
+    num = 0
+    for items in range(x):
         try:
-            if i > a:
-                raise Exception("Too far")
-            else:
-                result += a ** b / i
-        except Exception:
-            result = b + a
-            break
-    return result
+            print("{:d}".format(my_list[items]), end='')
+            num += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return num
