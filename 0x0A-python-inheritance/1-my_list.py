9655153554f3d5c8f1e7bd744dbbe96ff8dde057
Babaoni147 <obahedwin@ymail.com>
2024-04-16 21:14:53 +0100

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-'''Module for MyList class.'''
+
+"""Module for MyList class."""
 
 class MyList(list):
-    '''Custom MyList class.'''
+    """Custom MyList class."""
     def print_sorted(self):
-        '''Method for printing sorted list in ascending order.'''
+        """Method for printing sorted list in ascending order."""
         print(sorted(self))
