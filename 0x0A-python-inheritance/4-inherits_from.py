32cd753f50b66ba8cff7c827bb0123734c005512
Babaoni147 <obahedwin@ymail.com>
2024-04-16 21:46:42 +0100

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,7 @@
 def inherits_from(obj, a_class):
     '''Determines if an object is a true subclass of a class.'''
 
-    return isinstance(obj, a_class) and type(obj) != a_class
+    if isinstance(obj, a_class) and type(obj) != a_class:
+        return True
+    return False
+
