aa1092c2a1b3846e9653cfaa3752122baa2555f3
Babaoni147 <obahedwin@ymail.com>
2024-04-17 23:21:50 +0100

    11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -4,19 +4,24 @@
 
 
 class Student:
-    """Represent a student."""
+    """class with methods to_json for retrieves dictionary"""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        """method for initialized all atributes"""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Get a dictionary representation of the Student."""
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """method for retrieve a dictionary representation for a
+        student instance"""
+
+        if attrs is None:
+            return self.__dict__
+        dic = {}
+        for key, value in self.__dict__.items():
+            for i in attrs:
+                if key == i:
+                    dic[key] = value
+        return dic
