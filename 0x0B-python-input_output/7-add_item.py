80746cb7b15d823e5a86b4a57553578effa4049b
Babaoni147 <obahedwin@ymail.com>
2024-04-17 23:12:29 +0100

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Add all arguments to a Python list and save them to a file."""
+"""Add all arguments to a Python list and save them to a file."""``
 
 
 from sys import argv
@@ -14,7 +14,7 @@ try:
     list1 = list(load_from_json_file("add_item.json"))
 
 except:
-    len(list1) is 0
+    len(list1) == 0
 
 for arg in range(1, len(argv)):
     list1.append(argv[arg])
