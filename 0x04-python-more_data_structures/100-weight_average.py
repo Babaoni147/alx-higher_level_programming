b9789788532138df1b8cd100a21844b17f6fadfb
Babaoni147 <obahedwin@ymail.com>
2024-11-19 22:33:48 +0100

    updated

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda col: col**2, row)), matrix))
+"""Model that calculate the weighted average of all integers tuple"""
+
+
+def weight_average(my_list=[]):
+    """Function that return the weighted average of all integer tuple"""
+    if not my_list:  # Check if the list is empty
+        return 0
+
+    total_weighted_score = 0
+    total_weight = 0
+
+    for score, weight in my_list:
+        total_weighted_score += score * weight
+        total_weight += weight
+
+    # Avoid division by zero (though weights being 0 is unlikely given the problem)
+    if total_weight == 0:
+        return 0
+
+    return total_weighted_score / total_weight
